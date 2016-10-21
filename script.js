707d50cef55588317aeeff5d1886dc023b0e81af
Andrew <ochero1995@user.noreply.github.com>
2016-10-21 14:41:37 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $(document).keydown(function(key) {
+        switch(parseInt(key.which,10)) {
+            // Left arrow 
+            case 37:
+                $('img').animate({left: "-=10px"}, 'fast');
+                break;
+            // Up Arrow 
+            case 38:
+                $('img').animate({top: "-=10px"}, 'fast');
+                break;
+            // Right Arrow 
+            case 39:
+                $('img').animate({left: "+=10px"}, 'fast');
+                break;
+            // Down Arrow 
+            case 40:
+                $('img').animate({top: "+=10px"}, 'fast');
+                break;
+        }
+    });
+});
